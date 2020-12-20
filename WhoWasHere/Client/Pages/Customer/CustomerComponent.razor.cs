d990fde65d626f2b18a06c1ab1ba0c42f8a7dca1
fbicciato <f.bicciato.lavoro@gmail.com>
2020-12-20 17:34:19 +0100

    -Add navbar referance -Add customer component

---
+++ WhoWasHere/Client/Pages/Customer/CustomerComponent.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhoWasHere.Client.Pages.Customer
+{
+    public partial class CustomerComponent : ComponentBase
+    {
+        public string FullName { get; set; } 
+        public string Address { get; set; } 
+        public long Number { get; set; } 
+        private DateTime LastVisit { get; set; } 
+        public string LastVisitDay { get;  } 
+
+
+
+    }
+}
